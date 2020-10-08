0282a71770856a96f3c5c52b3a933c3076cc9d18
Brett Henderson <bretth18@gmail.com>
2020-10-08 12:59:36 -0700

    Initial Commit

---
+++ Stemo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Stemo
+//
+//  Created by Brett Henderson on 10/8/20.
+//  Copyright © 2020 Brett Henderson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
